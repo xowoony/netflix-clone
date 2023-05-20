7378fa64484af6edc177a17a2e439f9e8225fade
xowoony <trumanblack@hanmail.net>
2023-05-20 21:51:30 +0900

    title, overview

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,13 +1,68 @@
 import { useQuery } from "react-query";
-import { getMovies } from "./api";
+import { IGetMoviesResult, getMovies } from "./api";
+import { styled } from "styled-components";
+
+const Wrapper = styled.div`
+  background-color: black;
+`;
+
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Banner = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+`;
+
+const Title = styled.h2`
+  font-size: 68px;
+  font-family: 'Carter One', cursive;
+  width: 40%;
+`;
+
+
+const Overview = styled.p`
+    color: #fff;
+    font-size: 1.2vw;
+    font-weight: 400;
+    line-height: normal;
+    margin-top: 0.1vw;
+    text-shadow: 2px 2px 4px rgba(0,0,0,.45);
+    width: 40%;
+`;
 
 function Home() {
   // useQuery
   // 기본적으로 key를 제공해주어야 한다. (문자열 or 배열)
   // getMovies 로 api를 가져옴.
-  const { data, isLoading } = useQuery(["movies", "nowPlaying"], getMovies); // movies, nowPlaying => 식별자
+  // 타입스크립트에게 useQuery의 결과가 IGetMoviesResult 타입이라고 알려주기
+  const { data, isLoading } = useQuery<IGetMoviesResult>(
+    ["movies", "nowPlaying"],
+    getMovies
+  ); // movies, nowPlaying => 식별자
   console.log(data, isLoading);
-  return <div style={{ height: "200vh" }}>Home</div>;
+  // <></> 공통된 부모 없이 연이어 리턴하기
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner>
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+        </>
+      )}
+    </Wrapper>
+  );
 }
 
 export default Home;
--- src/Routes/api.ts
+++ src/Routes/api.ts
@@ -1,6 +1,28 @@
 const API_KEY = "e03a93d879218d0e8080eba916c8afdf";
 const BASE_PATH = "https://api.themoviedb.org/3/";
 
+// IMovie 인터페이스
+interface IMovie {
+  id:number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+  // video: boolean;
+}
+
+// GetMoviesResult(getMovies의 결과) 인터페이스 객체
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
 // api fetch
 export function getMovies() {
   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
