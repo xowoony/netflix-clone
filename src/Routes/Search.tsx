371e68af9598dfc5b3bc1b38fbf317d132ccdb44
xowoony <trumanblack@hanmail.net>
2023-06-08 14:15:50 +0900

    search 추가

--- src/Routes/Search.tsx
+++ src/Routes/Search.tsx
@@ -1,5 +1,9 @@
-function Search() {
-  return <h1>Search</h1>;
-}
+import { useLocation } from "react-router";
 
+function Search() {
+  const location = useLocation();
+  const keyword = new URLSearchParams(location.search).get("keyword");
+  console.log(keyword);
+  return null;
+}
 export default Search;
